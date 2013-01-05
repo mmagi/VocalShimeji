3951910856feffb71f0d2ad7687cc46d58097919
Magi <mmagics@gmail.com>
2013-01-05 22:37:31 +0800

    调整窗体绘制对native api的调用，paint方法的调用时间缩短50%以上。#1

--- src_win/com/group_finity/mascot/win/WindowsTranslucentWindow.java
+++ src_win/com/group_finity/mascot/win/WindowsTranslucentWindow.java
@@ -16,7 +16,6 @@ import java.awt.*;
  * α値つき画像ウィンドウ.
  * {@link #setImage(com.group_finity.mascot.image.NativeImage)} で設定した {@link WindowsNativeImage} をデスクトップに表示できる.
  * <p/>
- * {@link #setAlpha(int)} で表示するときの濃度も指定できる.
  */
 class WindowsTranslucentWindow extends JWindow implements TranslucentWindow {
 
@@ -26,79 +25,89 @@ class WindowsTranslucentWindow extends JWindow implements TranslucentWindow {
     public JWindow asJWindow() {
         return this;
     }
-
-    /**
-     * α値つき画像を描画する.
-     *
-     * @param imageHandle ビットマップのハンドル.
-     * @param alpha       表示濃度. 0 = まったく表示しない、255 = 完全に表示する.
-     */
-    private void paint(final WinDef.HBITMAP imageHandle, final int alpha) {
-
-        final WinDef.HWND hWnd = new WinDef.HWND(Native.getComponentPointer(this));
-
-        if (User32Ex.INSTANCE.IsWindowVisible(hWnd)) {
-
-            final int exStyle = User32Ex.INSTANCE.GetWindowLongW(hWnd, WinUser.GWL_EXSTYLE);
-            if ((exStyle & WinUser.WS_EX_LAYERED) == 0) {
-                User32Ex.INSTANCE.SetWindowLongW(hWnd, WinUser.GWL_EXSTYLE, exStyle | WinUser.WS_EX_LAYERED);
-            }
-
-            // 画像の転送元DCを作成
-            final WinDef.HDC clientDC = User32Ex.INSTANCE.GetDC(hWnd);
-            final WinDef.HDC memDC = GDI32Ex.INSTANCE.CreateCompatibleDC(clientDC);
-            final WinNT.HANDLE oldBmp = GDI32Ex.INSTANCE.SelectObject(memDC, imageHandle);
-
-            User32Ex.INSTANCE.ReleaseDC(hWnd, clientDC);
-
-            // 転送先領域
-            final WinDef.RECT windowRect = new WinDef.RECT();
-            User32Ex.INSTANCE.GetWindowRect(hWnd, windowRect);
-
-            // 転送
-            final WinUser.BLENDFUNCTION bf = new WinUser.BLENDFUNCTION();
-            bf.BlendOp = WinUser.AC_SRC_OVER;
-            bf.BlendFlags = 0;
-            bf.SourceConstantAlpha = (byte) alpha; // 濃度を設定
-            bf.AlphaFormat = WinUser.AC_SRC_ALPHA;
-
-            final WinUser.POINT lt = new WinUser.POINT();
-            lt.x = windowRect.left;
-            lt.y = windowRect.top;
-            final WinUser.SIZE size = new WinUser.SIZE();
-            size.cx = windowRect.right - windowRect.left;
-            size.cy = windowRect.bottom - windowRect.top;
-            final WinUser.POINT zero = new WinUser.POINT();
-            User32Ex.INSTANCE.UpdateLayeredWindow(hWnd, null, lt, size, memDC, zero, 0, bf, WinUser.ULW_ALPHA);
-
-            // ビットマップは元に戻しておく
-            GDI32Ex.INSTANCE.SelectObject(memDC, oldBmp);
-
-            GDI32Ex.INSTANCE.DeleteDC(memDC);
-        }
-
+    final static WinUser.BLENDFUNCTION blendFunction = new WinUser.BLENDFUNCTION();
+    static {
+        blendFunction.BlendOp = WinUser.AC_SRC_OVER;
+        blendFunction.BlendFlags = 0;
+        blendFunction.SourceConstantAlpha = (byte) 255; // 濃度を設定
+        blendFunction.AlphaFormat = WinUser.AC_SRC_ALPHA;
     }
+    final WinUser.POINT pptSrc = new WinUser.POINT(); //zero
+
 
     /**
      * 表示する画像.
      */
     private WindowsNativeImage image;
 
-    /**
-     * 表示濃度. 0 = まったく表示しない、255 = 完全に表示する.
-     */
-    private int alpha = 255;
-
     @Override
     public String toString() {
         return "LayeredWindow[hashCode=" + hashCode() + ",bounds=" + getBounds() + "]";
     }
-
+    WinDef.HWND hWnd;
+    WinDef.HDC clientDC;
+    WinDef.HDC memDC;
     @Override
     public void paint(final Graphics g) {
         if (getImage() != null) {
-            // JNI を使用してα値つき画像を描画する.
-            paint(getImage().getHandle(), getAlpha());
+            final WinDef.HBITMAP imageHandle = getImage().getHandle();
+            if (null == hWnd){
+               hWnd = new WinDef.HWND(Native.getComponentPointer(this));
+            }
+            //final WinDef.HWND hWnd = new WinDef.HWND(Native.getComponentPointer(this));
+            if (User32Ex.INSTANCE.IsWindowVisible(hWnd)) {
+
+                final int exStyle = User32Ex.INSTANCE.GetWindowLongW(hWnd, WinUser.GWL_EXSTYLE);
+                if ((exStyle & WinUser.WS_EX_LAYERED) == 0) {
+                    User32Ex.INSTANCE.SetWindowLongW(hWnd, WinUser.GWL_EXSTYLE, exStyle | WinUser.WS_EX_LAYERED);
+                }
+
+                // 画像の転送元DCを作成
+                if (null == clientDC){
+                    clientDC = User32Ex.INSTANCE.GetDC(hWnd);
+                }
+                //final WinDef.HDC clientDC = User32Ex.INSTANCE.GetDC(hWnd);
+                if(null == memDC){
+                    memDC = GDI32Ex.INSTANCE.CreateCompatibleDC(clientDC);
+                }
+                //final WinDef.HDC memDC = GDI32Ex.INSTANCE.CreateCompatibleDC(clientDC);
+                final WinNT.HANDLE oldBmp = GDI32Ex.INSTANCE.SelectObject(memDC, imageHandle);
+
+                //User32Ex.INSTANCE.ReleaseDC(hWnd, clientDC);
+
+                // 転送先領域
+//                final WinDef.RECT windowRect = new WinDef.RECT();
+//                User32Ex.INSTANCE.GetWindowRect(hWnd, windowRect);
+
+                // 転送
+                //            final WinUser.POINT lt = new WinUser.POINT();
+                //            lt.x = windowRect.left;
+                //            lt.y = windowRect.top;
+                /**
+                 * 位置和大小都不在这里处理，全部传null
+                 *
+                 * BOOL WINAPI UpdateLayeredWindow(
+                 * _In_      HWND hwnd,
+                 * _In_opt_  HDC hdcDst,
+                 * _In_opt_  POINT *pptDst,
+                 * _In_opt_  SIZE *psize,
+                 * _In_opt_  HDC hdcSrc,
+                 * _In_opt_  POINT *pptSrc,
+                 * _In_      COLORREF crKey,
+                 * _In_opt_  BLENDFUNCTION *pblend,
+                 * _In_      DWORD dwFlags
+                 * );
+                 */
+                User32Ex.INSTANCE.UpdateLayeredWindow(hWnd, null, null, size, memDC, pptSrc, 0x00000000, blendFunction, WinUser.ULW_ALPHA);
+
+                // ビットマップは元に戻しておく
+                GDI32Ex.INSTANCE.SelectObject(memDC, oldBmp);
+                //An application cannot select a single bitmap into more than one DC at a time.
+                //释放掉当前图像,给其他mascot用
+
+                //GDI32Ex.INSTANCE.DeleteDC(memDC);
+            }
+
         }
     }
 
@@ -106,19 +115,14 @@ class WindowsTranslucentWindow extends JWindow implements TranslucentWindow {
         return this.image;
     }
     private boolean needRepaint = false;
+    final WinUser.SIZE size = new WinUser.SIZE();
     public void setImage(final NativeImage image) {
         if (image != this.image){
             this.image = (WindowsNativeImage) image;
-            needRepaint=true;
+            size.cx = this.image.getWidth();
+            size.cy = this.image.getHeight();
+            needRepaint = true;
         }
-    }
-
-    public int getAlpha() {
-        return this.alpha;
-    }
-
-    public void setAlpha(final int alpha) {
-        this.alpha = alpha;
     }
 
     public void updateImage() {
